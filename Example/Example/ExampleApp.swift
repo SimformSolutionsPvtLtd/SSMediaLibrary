5220e2d605876bdff2a60deb6954d72d803df737
Devansh Vyas <devansh.v@simformsolutions.com>
2021-09-29 22:40:50 +0530

    Added example again

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Devansh Vyas on 29/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/Pods/Target Support Files/SSMediaLibrary/SSMediaLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SSMediaLibrary : NSObject
+@end
+@implementation PodsDummy_SSMediaLibrary
+@end
